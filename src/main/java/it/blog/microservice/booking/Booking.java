9b85213e478e52ff5247c7f262615049bacab748
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-01-30 16:06:28 +0100

    Create Booking.java

---
+++ src/main/java/it/blog/microservice/booking/Booking.java
@@ -0,0 +1,60 @@
+package it.blog.microservice.booking;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+　
+public class Booking implements Serializable{
+
+	@Id
+	protected String code;
+	
+	protected String flightNumber;
+	
+	protected String name;
+	
+	protected String surname;
+	
+	protected String seat; 
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getSeat() {
+		return seat;
+	}
+
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+}
